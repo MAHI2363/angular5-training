aabeb96b4dda5df690ec585b203125445f577c1e
Jinto Jose <jintoppy@gmail.com>
2017-11-21 08:15:19 +0530

    Adding app component changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular5 Training';
+  selectedEmployee;
+  newEmployee: Employee = new Employee('', null);
+  employees: Array<Employee> = [
+    new Employee('Ajith', 23),
+    new Employee('Rajni', 33)
+  ];
+
+  clearSelections() {
+    this.employees.forEach(emp => emp.selected = false);
+  }
+
+  onSelectEmployee(employee: Employee) {
+    this.clearSelections();
+    employee.selected = true;
+    this.selectedEmployee = employee;
+  }
+
+  addEmployee() {
+    this.employees.push(this.newEmployee);
+  }
+
 }
